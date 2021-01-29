b664758cb70530c94d9f5a648d2b679009900254
田梓衡 <39756011+IveTian@users.noreply.github.com>
2021-01-29 13:50:23 +0800

    Added network status judgment

--- js/network.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function(){
-    if (navigator.onLine) {
-            window.location.href="./index.html";
-        } else {
-            window.location.href="./error.html";
-        }
-},800)
-
