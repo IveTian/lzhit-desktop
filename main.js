27e177a5533f3e7ec11ac8937604a07b0270697d
田梓衡 <39756011+IveTian@users.noreply.github.com>
2021-02-01 14:57:43 +0800

    Prevent opening multiple identical clients.

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ var BrowserWindow = electron.BrowserWindow // 窗口引用
 var mainWindow = null //声明要打开的主窗口
 
 app.on('ready',()=>{
+    //客户端多开报错
+    const gotTheLock = app.requestSingleInstanceLock()
+    if (!gotTheLock) {
+        app.quit()
+        electron.dialog.showErrorBox('启动失败','请检查是否已经打开了一个客户端！(code:020)');
+      }
+      //打开主窗体
     mainWindow = new BrowserWindow({
         width:500,
         height:870,
@@ -24,6 +31,7 @@ app.on('ready',()=>{
     mainWindow.on('closed',()=>{
         mainWindow = null //监听关闭操作，防止内存爆炸
     })
+    //开发者工具
     //mainWindow.webContents.openDevTools({mode:'detach'}); //默认关闭开发者工具
     
 })
